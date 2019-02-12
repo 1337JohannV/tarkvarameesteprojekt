a54b5268bac428c1e7ed9d337f5836c28c65a52c
Johann <joveis@ttu.ee>
2019-02-12 16:07:14 +0200

    Initial commit

---
+++ src/main/java/com/tarkvaramehed/projekt/tarkvarameesteprojekt/TarkvarameesteprojektApplication.java
@@ -0,0 +1,14 @@
+package com.tarkvaramehed.projekt.tarkvarameesteprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarkvarameesteprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarkvarameesteprojektApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tarkvaramehed/projekt/tarkvarameesteprojekt/TarkvarameesteprojektApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tarkvaramehed.projekt.tarkvarameesteprojekt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TarkvarameesteprojektApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
