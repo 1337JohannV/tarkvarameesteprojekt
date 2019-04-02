65d4080449a1b20b49c08b8a43253875cbfa8493
Priit Roosiväli <prroos@ttu.ee>
2019-04-03 01:56:44 +0300

    New next page logic( implements backend)

---
+++ web/vue-app/src/components/FilterBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id=filterBar>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'FilterBar',
+  data: function(){
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+#filterBar{
+    width: 30%;
+    height: 50vh;
+    border: 2px solid black;
+    position: sticky;
+    top: 0px;
+}
+</style>
