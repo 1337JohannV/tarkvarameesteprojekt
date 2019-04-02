528a6aec14a0018ba00528e3cf4856a324e800d7
heviir <heviir@ttu.ee>
2019-04-02 23:22:05 +0300

    scraped cheeses, fixed merger bugs and stuff

---
+++ src/main/java/scraper/util/thread/test/TestScraperThread.java
@@ -0,0 +1,31 @@
+package scraper.util.thread.test;
+
+import com.tarkvaramehed.projekt.tarkvarameesteprojekt.model.Product;
+import com.tarkvaramehed.projekt.tarkvarameesteprojekt.model.enums.Category;
+import scraper.Scraper;
+import scraper.util.thread.ScraperThread;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestScraperThread extends ScraperThread {
+
+    private String url;
+
+    public TestScraperThread(Scraper scraper, String url) {
+        super(scraper);
+        this.url = url;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("TEST SCRAPER RUNNING FOR SCRAPER:" + getScraper().toString());
+        setProducts(getScraper().getDemoData(Category.JUUSTUD));
+        //setProducts(Arrays.asList(getScraper().getProductFromPage(url)));
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        return super.getProducts();
+    }
+}
