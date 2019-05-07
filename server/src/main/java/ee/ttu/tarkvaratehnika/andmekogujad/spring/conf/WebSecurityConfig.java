73bfdf8ec7e14fd4f7243927a1ff4afdde79b378
Johann <joveis@ttu.ee>
2019-05-08 01:21:23 +0300

    andmed rdsis

--- server/src/main/java/ee/ttu/tarkvaratehnika/andmekogujad/spring/conf/WebSecurityConfig.java
+++ server/src/main/java/ee/ttu/tarkvaratehnika/andmekogujad/spring/conf/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -39,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:8081"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:8081","http://localhost:8081/ostukorv"));
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", config);
